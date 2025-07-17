0eda02c5a9e69914d75169f12dff007de61158e7
presi <preslavborisov@abv.bg>
2025-07-17 10:57:35 +0300

    Fixes: Class and Methods Naming, DataUtils Singleton, Exception handling, Session Manager Singleton, Model and Repo Unification

--- src/main/java/org/example/Ui/UserCommands/DeletingUserCommand.java
+++ src/main/java/org/example/Ui/UserCommands/DeletingUserCommand.java
@@ -37,29 +37,29 @@ public class DeletingUserCommand implements Command {
         System.out.println("Deleting user");
         System.out.println("Are you sure you want to delete this user?(Y/N)");
         String answer = scanner.nextLine();
-        if (answer.isEmpty()) {
-            System.err.println("Answer is empty");
-            return true;
-        }
-        if (answer.equals("Y")) {
-            System.out.println("Deleting projects created by " + users.getName());
-            List<Project> createdBy = projectUserService.getAllProjectsCreatedByUser(users.getId());
-            for (Project project : createdBy) {
-                System.out.print("Deleting project " + project.getName() + ": ");
-                boolean delete = projectService.deleteProject(project);
-                System.out.println(delete);
-            }
-
-            boolean deleted = userService.deleteUser(users.getId());
-            if (!deleted) {
-                System.err.println("Failed to delete user");
+        switch (answer) {
+            case "" -> {
+                System.err.println("Answer is empty");
                 return true;
-            } else {
-                sessionManager.logout();
             }
+            case "Y" -> {
+                System.out.println("Deleting projects created by " + users.getName());
+                List<Project> createdBy = projectUserService.getAllProjectsCreatedByUser(users.getId());
+                for (Project project : createdBy) {
+                    System.out.print("Deleting project " + project.getName() + ": ");
+                    boolean delete = projectService.deleteProject(project);
+                    System.out.println(delete);
+                }
 
-        } else if (answer.equals("N")) {
-            System.out.println("Deleting process is rejected");
+                boolean deleted = userService.deleteUser(users.getId());
+                if (!deleted) {
+                    System.err.println("Failed to delete user");
+                    return true;
+                } else {
+                    sessionManager.logout();
+                }
+            }
+            case "N" -> System.out.println("Deleting process is rejected");
         }
         return true;
     }
